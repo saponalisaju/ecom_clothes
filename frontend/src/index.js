0b2ab02d1c27b60fb3f94d2f939d9fc71f7c293e
saponalisaju <saponalibpl12486@gmail.com>
2025-09-26 19:02:36 +0600

    something

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
 import ShopContextProvider from "./Context/ShopContext";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes";
